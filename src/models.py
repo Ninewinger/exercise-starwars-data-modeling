85176277fa6a769eb6a15df7ad7a2b41088991ef
Ninewinger <d.v.petricio@live.com>
2021-09-17 00:05:12 +0000

    terminado

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,40 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False, unique= True)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique= True)
+    loginStatus = Column(String(50), nullable=False)
+    favorites = Column(Integer, ForeignKey('Favorite.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
+    def verifyLogin(self):
         return {}
+
+class Favorite(Base):
+    __tablename__ = 'Favorite'
+    id = Column(Integer, primary_key=True)
+    planet_id = Column(Integer, ForeignKey('Planets.id'))
+    character_id = Column(Integer, ForeignKey('Characters.id'))
+
+class Planets(Base):
+    __tablename__= "Planets"
+    id = Column(Integer, primary_key = True)
+    diameter = Column(String(250))
+    size = Column(String(250))
+    atmosphere = Column(String(250))
+    temperature = Column(String(250))
+  
+class Characters(Base):
+    __tablename__= "Characters"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250))
+    age = Column(String(250))
+    height = Column(String(250))
+    mass = Column(String(250))
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
